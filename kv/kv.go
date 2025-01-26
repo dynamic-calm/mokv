478145e00057984ac5a499e2b5e68eeddec5a2a5
Mateo Presa Castro <mateopresacastro@gmail.com>
2025-01-26 21:51:48 +0100

    feat: change run

--- kv/kv.go
+++ kv/kv.go
@@ -10,8 +10,10 @@ import (
 	"os/signal"
 	"time"
 
+	"github.com/mateopresacastro/mokv/api"
 	"github.com/mateopresacastro/mokv/auth"
 	"github.com/mateopresacastro/mokv/config"
+	"github.com/mateopresacastro/mokv/discovery"
 	"github.com/mateopresacastro/mokv/server"
 	"github.com/mateopresacastro/mokv/store"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -22,48 +24,37 @@ import (
 	"google.golang.org/grpc/stats/opentelemetry"
 )
 
+type kv struct {
+}
+
 func Run(ctx context.Context) error {
 	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt)
 	defer cancel()
 
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "3000"
-		slog.Warn("PORT not set", "using", port)
-	}
-	listener, err := net.Listen("tcp", ":"+port)
+	provider, err := setupMetricsServer(ctx)
 	if err != nil {
-		return fmt.Errorf("failed to listen: %w", err)
-	}
-	defer listener.Close()
-
-	serverTLSConfig, err := config.SetupTLSConfig(config.TLSConfig{
-		CertFile:      config.ServerCertFile,
-		KeyFile:       config.ServerKeyFile,
-		CAFile:        config.CAFile,
-		ServerAddress: listener.Addr().String(),
-		Server:        true,
-	})
-	if err != nil {
-		return fmt.Errorf("failed to setup TLS: %w", err)
+		return err
 	}
 
+	errChan, err := setupGRPCServer(ctx, provider)
+	if err != nil {
+		return err
+	}
+
+	<-ctx.Done()
+
+	select {
+	case err := <-errChan:
+		return err
+	default:
+		return nil
+	}
+}
+
+func setupMetricsServer(ctx context.Context) (*metric.MeterProvider, error) {
 	exporter, err := prometheus.New()
 	if err != nil {
-		return fmt.Errorf("failed to start prometheus exporter: %w", err)
-	}
-	provider := metric.NewMeterProvider(metric.WithReader(exporter))
-	defer provider.Shutdown(ctx)
-
-	serverOpts := []grpc.ServerOption{
-		grpc.Creds(credentials.NewTLS(serverTLSConfig)),
-		opentelemetry.ServerOption(
-			opentelemetry.Options{
-				MetricsOptions: opentelemetry.MetricsOptions{
-					MeterProvider: provider,
-				},
-			},
-		),
+		return nil, fmt.Errorf("failed to start prometheus exporter: %w", err)
 	}
 
 	metricsPort := os.Getenv("METRICS_PORT")
@@ -71,6 +62,10 @@ func Run(ctx context.Context) error {
 		metricsPort = "4000"
 		slog.Warn("METRICS_PORT not set", "using", metricsPort)
 	}
+
+	provider := metric.NewMeterProvider(metric.WithReader(exporter))
+	defer provider.Shutdown(ctx)
+
 	go func() {
 		ticker := time.NewTicker(5 * time.Second)
 		defer ticker.Stop()
@@ -89,13 +84,58 @@ func Run(ctx context.Context) error {
 		}
 	}()
 
+	return provider, nil
+}
+
+func setupGRPCServer(ctx context.Context, provider *metric.MeterProvider) (chan error, error) {
+	_, cancel := context.WithCancel(ctx)
+	defer cancel()
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+		slog.Warn("PORT not set", "using", port)
+	}
+
+	listener, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		return nil, fmt.Errorf("failed to listen: %w", err)
+	}
+
+	serverTLSConfig, err := config.SetupTLSConfig(config.TLSConfig{
+		CertFile:      config.ServerCertFile,
+		KeyFile:       config.ServerKeyFile,
+		CAFile:        config.CAFile,
+		ServerAddress: listener.Addr().String(),
+		Server:        true,
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to setup TLS: %w", err)
+	}
+
+	serverOpts := []grpc.ServerOption{
+		grpc.Creds(credentials.NewTLS(serverTLSConfig)),
+		opentelemetry.ServerOption(
+			opentelemetry.Options{
+				MetricsOptions: opentelemetry.MetricsOptions{
+					MeterProvider: provider,
+				},
+			},
+		),
+	}
+
 	store := store.New()
 	authorizer := auth.New(config.ACLModelFile, config.ACLPolicyFile)
 	server := server.New(store, authorizer, serverOpts...)
 
 	srvErrChan := make(chan error, 1)
+
 	go func() {
-		defer close(srvErrChan)
+		defer func() {
+			close(srvErrChan)
+			server.GracefulStop()
+			listener.Close()
+		}()
 		slog.Info("grpc server listening...", "addr", listener.Addr())
 		if err := server.Serve(listener); err != nil {
 			slog.Error("server error", "err", err)
@@ -104,15 +144,40 @@ func Run(ctx context.Context) error {
 		}
 	}()
 
-	<-ctx.Done()
-	slog.Info("shutting down server...")
-	server.GracefulStop()
-	slog.Info("server stopped gracefully")
+	return srvErrChan, nil
+}
 
-	select {
-	case err := <-srvErrChan:
-		return err
-	default:
-		return nil
+func setupMemership() (*discovery.Membership, error) {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
 	}
+	peerTLSConfig, err := config.SetupTLSConfig(config.TLSConfig{
+		CertFile:      config.RootClientCertFile,
+		KeyFile:       config.RootClientKeyFile,
+		CAFile:        config.CAFile,
+		ServerAddress: "localhost",
+	})
+	creds := credentials.NewTLS(peerTLSConfig)
+	opts := grpc.WithTransportCredentials(creds)
+	cc, err := grpc.NewClient(":"+port, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	client := api.NewKVClient(cc)
+	replicator := &Replicator{
+		DialOptions: []grpc.DialOption{opts},
+		LocalServer: client,
+	}
+	membership, err := discovery.New(replicator, discovery.Config{
+		NodeName: "test",
+		BindAddr: "test",
+		Tags: map[string]string{
+			"rpc_addr": ":" + port,
+		},
+		StartJoinAddrs: []string{"test"},
+	})
+
+	return membership, nil
 }
