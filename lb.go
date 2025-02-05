cddd470eab98025e444522b1d275d5a116a7ade2
Mateo Presa Castro <mateopresacastro@gmail.com>
2025-02-05 15:08:31 +0100

    chore: move lb + picker

--- lb.go
+++ lb.go
@@ -1,4 +1,4 @@
-package lb
+package mokv
 
 import (
 	"context"
--- run_test.go
+++ run_test.go
@@ -15,7 +15,6 @@ import (
 	"github.com/mateopresacastro/mokv"
 	"github.com/mateopresacastro/mokv/api"
 	"github.com/mateopresacastro/mokv/config"
-	"github.com/mateopresacastro/mokv/lb"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/protobuf/types/known/emptypb"
@@ -97,7 +96,7 @@ func TestRunE2E(t *testing.T) {
 	clientCreds := credentials.NewTLS(clientTLSConfig)
 	rpcAddr := "127.0.0.1:" + strconv.Itoa(cfg.RPCPort)
 	conn, err := grpc.NewClient(
-		fmt.Sprintf("%s:///%s", lb.Name, rpcAddr),
+		fmt.Sprintf("%s:///%s", mokv.Name, rpcAddr),
 		grpc.WithTransportCredentials(clientCreds),
 	)
 	if err != nil {
